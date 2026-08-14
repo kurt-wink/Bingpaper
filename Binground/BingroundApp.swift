b30b9f2807a6d2d3e759c3e6cfe7a67613cbae5b
Kurt <38019652+kurt-wink@users.noreply.github.com>
2026-08-15 10:29:00 +1200

    Initial Commit

---
+++ Binground/BingroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  BingroundApp.swift
+//  Binground
+//
+//  Created by Kurt on 15/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct BingroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BingroundTests/BingroundTests.swift
@@ -0,0 +1,17 @@
+//
+//  BingroundTests.swift
+//  BingroundTests
+//
+//  Created by Kurt on 15/08/2026.
+//
+
+import Testing
+@testable import Binground
+
+struct BingroundTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
